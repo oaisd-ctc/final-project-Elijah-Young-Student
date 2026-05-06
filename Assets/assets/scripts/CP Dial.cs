3f9cba4444e897fd4be35d33c62ce2c03356a0a1
Elijah-Young-Student <elyo1a0@student.oaisd.org>
2026-05-06 14:00:38 -0400

    blender more, UNITY YEAAAA

--- Assets/assets/scripts/CP Dial.cs
+++ Assets/assets/scripts/CP Dial.cs
@@ -29,37 +29,52 @@ public class CPDial : MonoBehaviour
         switch (value)
         {
             case CPValues.Zero:
-                targetRotation = Quaternion.Euler(0, 0, 0);
+                targetRotation = Quaternion.Euler(-90, -90, -90);
                 break;
             case CPValues.One:
+                targetRotation = Quaternion.Euler(-112.05f, -90, -90);
                 break;
             case CPValues.Two:
+                targetRotation = Quaternion.Euler(-134.1f, -90, -90);
                 break;
             case CPValues.Three:
+                targetRotation = Quaternion.Euler(-157.6f, -90, -90);
                 break;
             case CPValues.Four:
+                targetRotation = Quaternion.Euler(-179, -90, -90);
                 break;
             case CPValues.Five:
+                targetRotation = Quaternion.Euler(-203, -90, -90);
                 break;
             case CPValues.Six:
+                targetRotation = Quaternion.Euler(-225.05f, -90, -90);
                 break;
             case CPValues.Seven:
+                targetRotation = Quaternion.Euler(-247.95f, -90, -90);
                 break;
             case CPValues.Eight:
+                targetRotation = Quaternion.Euler(-269.35f, -90, -90);
                 break;
             case CPValues.Nine:
+                targetRotation = Quaternion.Euler(-292.05f, -90, -90);
                 break;
             case CPValues.Ten:
+                targetRotation = Quaternion.Euler(-313.08f, -90, -90);
                 break;
             case CPValues.Eleven:
+                targetRotation = Quaternion.Euler(-336.4f, -90, -90);
                 break;
             case CPValues.Twelve:
+                targetRotation = Quaternion.Euler(-375.3f, -90, -90);
                 break;
             case CPValues.Thirteen:
+                targetRotation = Quaternion.Euler(-381.026f, -90, -90);
                 break;
             case CPValues.Fourteen:
+                targetRotation = Quaternion.Euler(-403.2f, -90, -90);
                 break;
             case CPValues.Fifteen:
+                targetRotation = Quaternion.Euler(-425.617f, -90, -90);
                 break;
         }
     }
