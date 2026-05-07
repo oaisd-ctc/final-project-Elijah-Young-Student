9fc4a45f477a3ace079349bf6534b0bb488c60bf
Elijah-Young-Student <elyo1a0@student.oaisd.org>
2026-05-07 14:00:22 -0400

    Animation For Camera :)

--- Assets/assets/scripts/Character Sheet Flip.cs
+++ Assets/assets/scripts/Character Sheet Flip.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CharacterSheetFlip : MonoBehaviour
 {
+
+    public Animator anima;
+    public bool flipped = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anima = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        flipped = !flipped;
+        if (flipped == true) 
+            anima.SetBool("Flipped", true);
+        else if (flipped == false)
+            anima.SetBool("Flipped", false);
     }
 }
